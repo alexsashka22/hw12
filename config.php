6be9ad6425a8bcb303e2c6198773bf55f3a9d722
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-03-31 16:41:04 +0300

    2 var

--- config.php
+++ config.php
@@ -7,4 +7,11 @@ $bd = "hw12";
 
 $connect = mysqli_connect($server, $login, $pass, $bd);
 
+// try {
+//     $pdo = new PDO('mysql:host=localhost;dbname=hw12', 'root', '');
+//     }
+//     catch (PDOException $e) {
+//         die("Error!: " . $e->getMessage());
+//     }
+
  ?>
